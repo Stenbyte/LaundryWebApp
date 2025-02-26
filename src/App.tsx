beb81f3a37991d5cbd3a0a15f86cc43eea31af07
Sten Scheifel <sten.scheifel@gmail.com>
2025-02-26 21:54:30 +0100

    added QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import { BookingTable } from './components/BookingTable'
+import { Container, Paper } from '@mui/material'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 function App() {
 
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <Container maxWidth="xl" component={Paper}>LB</Container>
       <BookingTable />
-    </>
+    </QueryClientProvider>
   )
 }
 
