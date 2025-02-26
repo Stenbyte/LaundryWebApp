e9946da7e854dd2921d5dd9408de3355fbeed048
Sten Scheifel <sten.scheifel@gmail.com>
2025-02-26 21:55:14 +0100

    first setup for week slots and time

--- src/components/BookingTable.tsx
+++ src/components/BookingTable.tsx
@@ -1,4 +1,5 @@
 
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import dayjs from "dayjs";
 
 const TIME_SLOTS = ['08:00-11:00', "11:00-14:00", "14:00-17:00", "17:00-20:00"];
@@ -7,5 +8,43 @@ export function BookingTable() {
 
     const today = dayjs();
     const weekDays = Array.from({ length: 7 }, (_, i) => today.add(i, "day").format("YYYY-MM-DD"))
-    return (<>{weekDays}</>)
+
+    const isReserved = (date, timeSlot) => {
+        return [].some((booking) => booking.date === date && booking.timeSlot === timeSlot)
+    }
+    return (<div>
+        <Typography variant="h4" align="center" gutterBottom>
+            Book Time
+        </Typography>
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Time Slot</TableCell>
+                        {weekDays.map((day) => (
+                            <TableCell key={day} align="center">
+                                {dayjs(day).format("D ddd, MMM")}
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {TIME_SLOTS.map((slot) => (
+                        <TableRow key={slot}>
+                            <TableCell>{slot}</TableCell>
+                            {weekDays.map((day) => (
+                                <TableCell key={day} align="center">
+                                    {isReserved(day, slot) ? (<Typography color="info">Booked</Typography>) : (
+                                        <Button variant="outlined" size="small" onClick={() => { }}>
+                                            Reserve
+                                        </Button>
+                                    )}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    </div>)
 }
