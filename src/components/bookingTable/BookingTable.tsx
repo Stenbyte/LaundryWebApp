fe5db8ad0e408eb3aef49c9e1d9767290fb325e0
Sten Scheifel <sten.scheifel@gmail.com>
2025-02-27 22:09:07 +0100

    file location change

--- src/components/bookingTable/BookingTable.tsx
+++ src/components/bookingTable/BookingTable.tsx
@@ -1,21 +1,41 @@
 
-import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Button, Divider, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import dayjs from "dayjs";
+import { fetchBookings, reserveSlot } from "../../services/BookingService";
+import { BookingCounter } from "./BookingCounter";
 
 const TIME_SLOTS = ['08:00-11:00', "11:00-14:00", "14:00-17:00", "17:00-20:00"];
 
 export function BookingTable() {
-
     const today = dayjs();
     const weekDays = Array.from({ length: 7 }, (_, i) => today.add(i, "day").format("YYYY-MM-DD"))
+
+    const queryClient = useQueryClient();
+    const { data: bookings = [], isLoading, isError } = useQuery({
+        queryKey: ["bookings"],
+        queryFn: fetchBookings
+    })
+
+    const mutation = useMutation({
+        mutationFn: ({ date, timeSlot }) => reserveSlot(date, timeSlot),
+        onSuccess: (data) => {
+            queryClient.invalidateQueries({ queryKey: ["bookings"] })
+            console.log('Data', data)
+        },
+        onError: (error) => {
+            console.log('Mutation failed', error)
+        }
+    })
+
+
 
     const isReserved = (date, timeSlot) => {
         return [].some((booking) => booking.date === date && booking.timeSlot === timeSlot)
     }
     return (<div>
-        <Typography variant="h4" align="center" gutterBottom>
-            Book Time
-        </Typography>
+        <BookingCounter />
+        <Divider />
         <TableContainer component={Paper}>
             <Table>
                 <TableHead>
