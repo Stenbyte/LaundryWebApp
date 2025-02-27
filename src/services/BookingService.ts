a4776520033d4f0553fb013b6f0e0005663fc2bf
Sten Scheifel <sten.scheifel@gmail.com>
2025-02-27 22:09:37 +0100

    importing config to use when sending request to db

--- src/services/BookingService.ts
+++ src/services/BookingService.ts
@@ -1,14 +1,13 @@
 import axios from "axios";
-
-const API_BASE_URL = "http://localhost:5063/api";
+import { Config } from "../../config";
 
 export const fetchBookings = async () => {
-  const { data } = await axios.get(`${API_BASE_URL}/bookings`);
+  const { data } = await axios.get(`${Config.API_BASE_URL}/bookings`);
   return data;
 };
 
 export const reserveSlot = async (date, timeSlot) => {
-  const { data } = await axios.post(`${API_BASE_URL}/bookings`, {
+  const { data } = await axios.post(`${Config.API_BASE_URL}/bookings`, {
     date,
     timeSlot,
   });
