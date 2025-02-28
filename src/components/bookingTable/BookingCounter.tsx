66ef27c5aa9b0816688db558649e532df926c573
Sten Scheifel <sten.scheifel@gmail.com>
2025-02-28 16:44:47 +0100

    added css classes fir booking counter

--- src/components/bookingTable/BookingCounter.tsx
+++ src/components/bookingTable/BookingCounter.tsx
@@ -1,27 +1,41 @@
 import { Box, Tooltip, Typography, Button } from "@mui/material";
 import InfoIcon from "@mui/icons-material/Info";
+import "../../App.css";
 
 export function BookingCounter() {
     return (
-        <Box
-            sx={{
-                display: "flex",
-                alignItems: "center",
-                // justifyContent: "space-between",
-                height: "50px",
-                width: "100vw",
-                margin: '20px 0'
-            }}
-        >
-            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-                <Tooltip title="Number of reservations that can be used per week">
+        <Box className="booking-counter-mainBox">
+            <Box className="booking-counter-resesrvation">
+                <Tooltip
+                    title="Number of reservations that can be used per week"
+                    color="info"
+                    placement="top-start"
+                >
                     <InfoIcon />
                 </Tooltip>
                 <Typography>Reservations left: 3</Typography>
             </Box>
-            <Box sx={{ padding: '10px', display: 'flex', justifyContent: 'space-between', width: '150px' }}>
-                <Button color="primary" size="small" variant="contained">Edit</Button>
-                <Button color="secondary" size="small" variant="outlined">Cancel</Button>
+            <Box className="booking-counter-edit-cancel-btn">
+                <Button color="primary" size="small" variant="contained">
+                    Edit
+                </Button>
+                <Button color="secondary" size="small" variant="outlined">
+                    Cancel
+                </Button>
+            </Box>
+            <Box
+                className='booking-counter-trade-btn'
+            >
+                <Tooltip
+                    title="Here you can trade your time slot with another tenants"
+                    placement="top"
+                    color="info"
+                >
+                    <InfoIcon />
+                </Tooltip>
+                <Button color="success" size="small" variant="contained">
+                    Trade
+                </Button>
             </Box>
         </Box>
     );
