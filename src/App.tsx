c94fd863bad6da874ec60a6e6c39cdb3b0a97df7
Sten Scheifel <sten.scheifel@gmail.com>
2025-02-28 21:48:33 +0100

    removing blank spaces

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,8 @@ const queryClient = new QueryClient();
 
 function App() {
 
-
   return (
     <ThemeProvider theme={theme}>
-
-
       <QueryClientProvider client={queryClient}>
         <Header />
         <BookingTable />
