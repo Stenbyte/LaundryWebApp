8371425a129d7262cc872bb70546d89042c69e67
Sten Scheifel <sten.scheifel@gmail.com>
2025-03-02 09:13:03 +0100

    Login component small refactor

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -8,39 +8,38 @@ import {
   DialogActions,
 } from "@mui/material";
 import { MetaDataType } from "../header/Header";
+import { GenericButton } from "../buttons/GenericButton";
 
 export function Login({
   data,
 }: {
-  data: Pick<
-    MetaDataType,
-    "isLoginOpen" | "setIsLoginOpen" | "setIsSignupOpen"
-  >;
+  data: Pick<MetaDataType, "setIsSignupOpen" | "isLogedIn" | "setIsLogedIn">;
 }) {
-  const { isLoginOpen, setIsLoginOpen, setIsSignupOpen } = data;
+  const { setIsSignupOpen, isLogedIn, setIsLogedIn } = data;
   return (
-    <Dialog open={isLoginOpen} onClose={() => setIsLoginOpen(false)}>
+    <Dialog open={!isLogedIn} hideBackdrop={true}>
       <DialogTitle>Login</DialogTitle>
       <DialogContent>
         <TextField fullWidth label="Email" margin="dense" />
         <TextField fullWidth label="Password" type="password" margin="dense" />
         <Typography variant="body2" sx={{ mt: 2 }}>
-          Don't have an account?{" "}
-          <Button
+          Don't have an account?
+          <GenericButton
+            children="Sign up"
             onClick={() => {
               setIsSignupOpen(true);
-              setIsLoginOpen(false);
             }}
-          >
-            Sign up
-          </Button>
+          />
         </Typography>
       </DialogContent>
       <DialogActions>
-        <Button onClick={() => setIsLoginOpen(false)} color="secondary">
-          Cancel
-        </Button>
-        <Button color="primary" variant="contained">
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={() => {
+            setIsLogedIn(true);
+          }}
+        >
           Login
         </Button>
       </DialogActions>
