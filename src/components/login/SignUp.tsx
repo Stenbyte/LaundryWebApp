7e2a816f7f115e9585f9b7dac7bf2abef68c5f3b
Sten Scheifel <sten.scheifel@gmail.com>
2025-03-09 20:43:11 +0100

    added toast, refined validation logic for signup, added few more data inputs

--- src/components/login/SignUp.tsx
+++ src/components/login/SignUp.tsx
@@ -12,6 +12,7 @@ import { object, string, InferType } from "yup";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 interface SignUpProps {
   data: {
@@ -21,6 +22,8 @@ interface SignUpProps {
 }
 
 const SubmitSchema = object().shape({
+  firstName: string().required("First name is required"),
+  lastName: string().required("Last name is required"),
   email: string()
     .email("Invalid email")
     .matches(
@@ -29,18 +32,48 @@ const SubmitSchema = object().shape({
     )
     .required("Email is required"),
   password: string()
-    // .matches(
-    //   /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-    //   "Password must be at least 8 characters long, include one uppercase letter, one lowercase letter, one number, and one special character"
-    // )
+    .matches(
+      /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      "Password must be at least 8 characters long, include one uppercase letter, one lowercase letter, one number, and one special character"
+    )
     .required("Password is required"),
-  phone: string()
-    .matches(/^\d{8,15}$/, "Invalid phone number")
-    .required("Phone is required"),
-  streetAdress: string()
-    .min(5, "Address must be at least 5 characters")
-    .required("Street Adress is required"),
+  phoneNumber: object({
+    number: string()
+      .matches(/^\d{8,15}$/, "Invalid phone number")
+      .required("Phone is required"),
+    countryCode: string()
+      .matches(/^\+\d{1,4}$/, "Invalid country code")
+      .required("Country Code is required"),
+  }),
+  adress: object({
+    streetName: string()
+      .min(5, "Address must be at least 5 characters")
+      .required("Street name is required"),
+    houseNumber: string()
+      .matches(
+        /^[1-9]\d*$/,
+        "House number must be a positive number greater than zero"
+      )
+      .required("House number is required"),
+  }),
 });
+
+const defaultSignUpValues: SignUpType = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  adress: {
+    streetName: "",
+    houseNumber: "",
+  },
+  phoneNumber: {
+    number: "",
+    countryCode: "",
+  },
+  password: "",
+};
+
+type SignUpType = InferType<typeof SubmitSchema>;
 
 export function SignUp({ data }: SignUpProps) {
   const { isSignupOpen, setIsSignupOpen } = data;
@@ -50,6 +83,7 @@ export function SignUp({ data }: SignUpProps) {
     control,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm({
     resolver: yupResolver(SubmitSchema),
   });
@@ -60,100 +94,170 @@ export function SignUp({ data }: SignUpProps) {
       return axios.post("http://localhost:5063/api/signup", formData);
     },
     onSuccess: () => {
-      alert("Sign-up successful!");
+      toast("Sign-up successful!");
       setIsSignupOpen(false);
       setIsLoading(false);
+      reset(defaultSignUpValues);
     },
     onError: (error) => {
-      alert("Sign-up failed. Please try again.");
+      toast("Sign-up failed.");
       setIsLoading(false);
       console.error(error);
     },
   });
-
-  type SignUpType = InferType<typeof SubmitSchema>;
 
   const onSubmit = (data: SignUpType) => {
     mutation.mutate(data);
   };
 
   return (
-    <Dialog open={isSignupOpen} onClose={() => setIsSignupOpen(false)}>
-      <DialogTitle>Sign Up</DialogTitle>
-      <DialogContent>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Controller
-            name="email"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="Email"
-                fullWidth
-                margin="dense"
-                error={!!errors.email}
-                helperText={errors.email?.message}
-              />
-            )}
-          />
-          <Controller
-            name="password"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="Password"
-                type="password"
-                fullWidth
-                margin="dense"
-                error={!!errors.password}
-                helperText={errors.password?.message}
-              />
-            )}
-          />
-          <Controller
-            name="phone"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="Phone"
-                fullWidth
-                margin="dense"
-                error={!!errors.phone}
-                helperText={errors.phone?.message}
-              />
-            )}
-          />
-          <Controller
-            name="streetAdress"
-            control={control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label="Street Adress"
-                fullWidth
-                margin="dense"
-                error={!!errors.streetAdress}
-                helperText={errors.streetAdress?.message}
-              />
-            )}
-          />
-          <DialogActions>
-            <Button onClick={() => setIsSignupOpen(false)} color="secondary">
-              Cancel
-            </Button>
-            <Button
-              type="submit"
-              color="primary"
-              variant="contained"
-              disabled={isLoading}
-            >
-              {isLoading ? "Signing Up..." : "Sign Up"}
-            </Button>
-          </DialogActions>
-        </form>
-      </DialogContent>
-    </Dialog>
+    <>
+      <ToastContainer />
+      <Dialog
+        open={isSignupOpen}
+        onClose={() => {
+          reset(defaultSignUpValues);
+          setIsSignupOpen(false);
+        }}
+      >
+        <DialogTitle>Sign Up</DialogTitle>
+        <DialogContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+              name="firstName"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="firstName"
+                  fullWidth
+                  margin="normal"
+                  error={!!errors.firstName}
+                  helperText={errors.firstName?.message}
+                />
+              )}
+            />
+            <Controller
+              name="lastName"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="lastName"
+                  fullWidth
+                  margin="normal"
+                  error={!!errors.lastName}
+                  helperText={errors.lastName?.message}
+                />
+              )}
+            />
+            <Controller
+              name="email"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="Email"
+                  fullWidth
+                  margin="normal"
+                  error={!!errors.email}
+                  helperText={errors.email?.message}
+                />
+              )}
+            />
+            <Controller
+              name="password"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="Password"
+                  type="password"
+                  fullWidth
+                  margin="normal"
+                  error={!!errors.password}
+                  helperText={errors.password?.message}
+                />
+              )}
+            />
+            <Controller
+              name="phoneNumber.number"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="number"
+                  fullWidth
+                  margin="normal"
+                  error={!!errors.phoneNumber?.number}
+                  helperText={errors.phoneNumber?.number?.message}
+                />
+              )}
+            />
+            <Controller
+              name="phoneNumber.countryCode"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="country code"
+                  fullWidth
+                  margin="normal"
+                  error={!!errors.phoneNumber?.countryCode}
+                  helperText={errors.phoneNumber?.countryCode?.message}
+                />
+              )}
+            />
+            <Controller
+              name="adress.streetName"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="street name"
+                  fullWidth
+                  margin="normal"
+                  error={!!errors.adress?.streetName}
+                  helperText={errors.adress?.streetName?.message}
+                />
+              )}
+            />
+            <Controller
+              name="adress.houseNumber"
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  label="house number"
+                  fullWidth
+                  margin="normal"
+                  error={!!errors.adress?.houseNumber}
+                  helperText={errors.adress?.houseNumber?.message}
+                />
+              )}
+            />
+            <DialogActions>
+              <Button
+                onClick={() => {
+                  reset(defaultSignUpValues);
+                  setIsSignupOpen(false);
+                }}
+                color="secondary"
+              >
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                color="primary"
+                variant="contained"
+                disabled={isLoading}
+              >
+                {isLoading ? "Signing Up..." : "Sign Up"}
+              </Button>
+            </DialogActions>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
