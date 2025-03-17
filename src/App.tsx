8ffa5b98968afcf664f88240f8a2bef548732e14
Sten Scheifel <sten.scheifel@gmail.com>
2025-03-17 20:25:48 +0100

    setting interseptor when queryClient is ready

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,18 @@ import { BookingTable } from "./components/bookingTable/BookingTable";
 import { Header } from "./components/header/Header";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
-import { useState } from "react";
-import { useAuth } from "./hooks/useAuth";
+import { useEffect, useState } from "react";
+import { useAuth, useRefreshToken } from "./hooks/useAuth";
+import { setupAxiosInterceptors } from "./services/AxiosConfig";
+import { useQueryClient } from "@tanstack/react-query";
 
 function App() {
+  const refreshMutation = useRefreshToken();
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    setupAxiosInterceptors(queryClient, refreshMutation);
+  }, [refreshMutation, queryClient]);
   const { data: user, error } = useAuth();
   // console.log(user, "USER data");
   // console.log(error, "Here error");
