54d8857af8e1ee23ae8b16634b2da6f3f356df92
Sten Scheifel <sten.scheifel@gmail.com>
2025-03-20 21:42:52 +0100

    added import form Axios Config and ReserveSlot import

--- src/services/BookingService.ts
+++ src/services/BookingService.ts
@@ -1,15 +1,17 @@
-import axios from "axios";
+
 import { Config } from "../../config";
+import { ReserveSlot } from "../components/bookingTable/BookingTable";
+import api from '../services/AxiosConfig';
 
 export const fetchBookings = async () => {
-  const { data } = await axios.get(`${Config.API_BASE_URL}/bookings`);
+  const { data } = await api.get(`${Config.API_BASE_URL}/bookings`);
   return data;
 };
 
-export const reserveSlot = async (date, timeSlot) => {
-  const { data } = await axios.post(`${Config.API_BASE_URL}/bookings`, {
-    date,
-    timeSlot,
+export const reserveSlot = async ({ day, time }: ReserveSlot) => {
+  const { data } = await api.post(`${Config.API_BASE_URL}/bookings`, {
+    day,
+    time,
   });
   return data;
 };
