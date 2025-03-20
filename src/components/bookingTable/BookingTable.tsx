45f0c6485e08241a313bbd6899345fae2fa2749c
Sten Scheifel <sten.scheifel@gmail.com>
2025-03-20 21:44:03 +0100

    initial setup for reserving and fetching bookings, needs api code

--- src/components/bookingTable/BookingTable.tsx
+++ src/components/bookingTable/BookingTable.tsx
@@ -4,67 +4,101 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import dayjs from "dayjs";
 import { fetchBookings, reserveSlot } from "../../services/BookingService";
 import { BookingCounter } from "./BookingCounter";
+import { toast, ToastContainer } from "react-toastify";
 
-const TIME_SLOTS = ['08:00-11:00', "11:00-14:00", "14:00-17:00", "17:00-20:00"];
+const TIME_SLOTS = ["08:00-11:00", "11:00-14:00", "14:00-17:00", "17:00-20:00"];
+
+export interface ReserveSlot {
+  day: string;
+  time: string;
+}
 
 export function BookingTable() {
-    const today = dayjs();
-    const weekDays = Array.from({ length: 7 }, (_, i) => today.add(i, "day").format("YYYY-MM-DD"))
+  const today = dayjs();
+  const weekDays = Array.from({ length: 7 }, (_, i) =>
+    today.add(i, "day").format("YYYY-MM-DD")
+  );
 
-    const queryClient = useQueryClient();
-    const { data: bookings = [], isLoading, isError } = useQuery({
-        queryKey: ["bookings"],
-        queryFn: fetchBookings
-    })
+  const queryClient = useQueryClient();
 
-    const mutation = useMutation({
-        mutationFn: ({ date, timeSlot }) => reserveSlot(date, timeSlot),
-        onSuccess: (data) => {
-            queryClient.invalidateQueries({ queryKey: ["bookings"] })
-            console.log('Data', data)
-        },
-        onError: (error) => {
-            console.log('Mutation failed', error)
-        }
-    })
+  const {
+    data: bookings = [],
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["bookings"],
+    queryFn: fetchBookings,
+    retry: 0,
+  });
 
+  // need to check this loading
+  if (isLoading) {
+    toast("Fetching reservations");
+  }
+  //   if (isError) {
+  //     // toast("Failed to fetch reservations");
+  //   }
 
+  const mutation = useMutation({
+    mutationFn: ({ day, time }: ReserveSlot) => reserveSlot({ day, time }),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["bookings"] });
+      console.log("Data", data);
+    },
+    onError: (error) => {
+      console.log("Mutation failed", error);
+    },
+  });
 
-    const isReserved = (date, timeSlot) => {
-        return [].some((booking) => booking.date === date && booking.timeSlot === timeSlot)
-    }
-    return (<div>
-        <BookingCounter />
-        <Divider />
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Time Slot</TableCell>
-                        {weekDays.map((day) => (
-                            <TableCell key={day} align="center">
-                                {dayjs(day).format("D ddd, MMM")}
-                            </TableCell>
-                        ))}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {TIME_SLOTS.map((slot) => (
-                        <TableRow key={slot}>
-                            <TableCell>{slot}</TableCell>
-                            {weekDays.map((day) => (
-                                <TableCell key={day} align="center">
-                                    {isReserved(day, slot) ? (<Typography color="info">Booked</Typography>) : (
-                                        <Button className="reserveBtn" size="small" onClick={() => { }}>
-                                            Reserve
-                                        </Button>
-                                    )}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    </div>)
+  const isReserved = ({ day, time }: ReserveSlot) => {
+    return bookings.some(
+      (booking: ReserveSlot) => booking.day === day && booking.time === time
+    );
+  };
+  return (
+    <div>
+      <ToastContainer />
+      <BookingCounter />
+      <Divider />
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Time Slot</TableCell>
+              {weekDays.map((day) => (
+                <TableCell key={day} align="center">
+                  {dayjs(day).format("D ddd, MMM")}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {TIME_SLOTS.map((time) => (
+              <TableRow key={time}>
+                <TableCell>{time}</TableCell>
+                {weekDays.map((day) => (
+                  <TableCell key={day} align="center">
+                    {isReserved({ day, time }) ? (
+                      <Typography color="info">Booked</Typography>
+                    ) : (
+                      <Button
+                        className="reserveBtn"
+                        size="small"
+                        onClick={() => {
+                          console.log("Reserving:", { day, time });
+                          mutation.mutate({ day, time } as ReserveSlot);
+                        }}
+                      >
+                        Reserve
+                      </Button>
+                    )}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
