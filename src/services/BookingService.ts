9214e1ce9683d8dba1769c9ce4439e92e34b5763
Sten Scheifel <sten.scheifel@gmail.com>
2025-03-22 20:08:31 +0100

    destructuring slot object for proper req.body

--- src/services/BookingService.ts
+++ src/services/BookingService.ts
@@ -1,17 +1,23 @@
 
 import { Config } from "../../config";
-import { ReserveSlot } from "../components/bookingTable/BookingTable";
+import { BookingSlot } from "../components/bookingTable/BookingTable";
 import api from '../services/AxiosConfig';
 
 export const fetchBookings = async () => {
-  const { data } = await api.get(`${Config.API_BASE_URL}/bookings`);
+  const { data } = await api.get(`${Config.API_BASE_URL}/booking/getAll`);
   return data;
 };
 
-export const reserveSlot = async ({ day, time }: ReserveSlot) => {
-  const { data } = await api.post(`${Config.API_BASE_URL}/bookings`, {
-    day,
-    time,
-  });
+export const reserveSlot = async (slot: BookingSlot) => {
+
+  const { data } = await api.post(`${Config.API_BASE_URL}/booking/create`, {
+    ...slot
+  },
+    {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
   return data;
 };
