563ac3d0337b96d9fd76bff7571cf2fef2b946a1
Sten Scheifel <sten.scheifel@gmail.com>
2025-06-07 13:43:18 +0200

    mocking useLogin to not send request to api

--- src/tests/Login.test.tsx
+++ src/tests/Login.test.tsx
@@ -1,21 +1,24 @@
 import { describe, it, expect, vi } from "vitest";
-import { fireEvent, render, screen, waitFor, within } from "./test-util";
+import {
+  createMockUseQueryResult,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  within,
+} from "./test-util";
 import { App } from "../App";
 import userEvent from "@testing-library/user-event";
-
-
-vi.mock("../../hooks/useAuth", () => ({
-  useLogin: () => ({
-    mutateAsync: vi.fn().mockResolvedValue({}),
-  }),
-  useAuth: () => ({
-    data: null,
-  }),
-}));
-
+import * as auth from "../hooks/useAuth";
 
 describe("Login", () => {
   it("submit email", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    vi.spyOn(auth, "useLogin").mockImplementation((): any => {
+      return createMockUseQueryResult({
+        token: "token",
+      });
+    });
     render(<App />);
 
     const emailWrapper = await screen.findByTestId("email");
