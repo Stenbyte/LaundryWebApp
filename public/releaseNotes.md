2524c3a4c8502c69bf814151ba988f651655da9b
Sten Scheifel <sten.scheifel@gmail.com>
2025-07-31 23:24:34 +0200

    feat: add release notes component with recent changes

--- public/releaseNotes.md
+++
@@ -1,7 +0,0 @@
-#### 🗓️ July 29, 2025
-
-- Enabled Password validation
-
-#### 🗓️ July 28, 2025
-
-- Added Release Notes Dialog
---
+++ src/components/releaseNotes/releaseNotes.ts
@@ -0,0 +1,45 @@
+export const news =
+    [
+        {
+            date: 'July 29, 2025',
+            change: "Enabled Password validation"
+        }, {
+            date: 'July 28, 2025',
+            change: "Added Release Notes Dialog"
+        },
+        {
+            date: 'July 29, 2025',
+            change: "Enabled Password validation"
+        }, {
+            date: 'July 28, 2025',
+            change: "Added Release Notes Dialog"
+        },
+        {
+            date: 'July 29, 2025',
+            change: "Enabled Password validation"
+        }, {
+            date: 'July 28, 2025',
+            change: "Added Release Notes Dialog"
+        },
+        {
+            date: 'July 29, 2025',
+            change: "Enabled Password validation"
+        }, {
+            date: 'July 28, 2025',
+            change: "Added Release Notes Dialog"
+        },
+        {
+            date: 'July 29, 2025',
+            change: "Enabled Password validation"
+        }, {
+            date: 'July 28, 2025',
+            change: "Added Release Notes Dialog"
+        },
+        {
+            date: 'July 29, 2025',
+            change: "Enabled Password validation"
+        }, {
+            date: 'July 28, 2025',
+            change: "Added Release Notes Dialog"
+        }
+    ]
