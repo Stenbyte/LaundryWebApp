d333cb349247b7206dade925465659a8333fc0db
Sten Scheifel <sten.scheifel@gmail.com>
2025-08-28 21:46:58 +0200

    added new hook to fetch machines

--- src/hooks/ machineHooks.ts
+++ src/hooks/ machineHooks.ts
@@ -1,16 +1,26 @@
 import { useQuery } from "@tanstack/react-query"
 import { Machine } from "../constants"
+import api from '../services/AxiosConfig';
+import { Config } from "../../config";
+import { AxiosError } from "axios";
 
 
-export const useFetchMachines = (userId: string) => {
+export const useFetchMachines = () => {
     return useQuery({
         queryKey: ["machines"],
-        queryFn: async (): Promise<Machine[]>=> {
-            try{
+        queryFn: async (): Promise<Machine[]> => {
+            try {
+                const getMachines = await api.get(`${Config.API_BASE_URL}/api/booking/getAllMachines`)
+                return getMachines.data as Machine[];
+            } catch (error: unknown) {
+                if (error instanceof AxiosError) {
+                    throw new Error(`Failed to fetch machines: ${error.message} (status: ${error.response?.status})`);
+                }
 
-            }catch (err){
-                throw new Error()
+                throw new Error("Unexpected error while fetching machines");
             }
+
         }
+
     })
 }
